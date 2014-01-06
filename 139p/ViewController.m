482278e4c28288e0819308c5457b0bac9c647602
SeungyoupBaek <bsy8220@naver.com>
2014-01-06 13:33:21 +0900

    139p 실습 완료했습니다.
    139p 실습 완료했습니다.

--- 139p/ViewController.m
+++ 139p/ViewController.m
@@ -9,10 +9,26 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *yearField;
+@property (weak, nonatomic) IBOutlet UITextField *monthField;
+@property (weak, nonatomic) IBOutlet UITextField *dayField;
 
 @end
 
 @implementation ViewController
+
+-(IBAction)pickerChanged:(id)sender{
+    NSLog(@"pickerChanged");
+    NSDate *selectedDate = self.datePicker.date;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger flag = NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit;
+    NSDateComponents *comp = [calendar components:flag fromDate:selectedDate];
+    self.yearField.text = [NSString stringWithFormat:@"%d", [comp year]];
+    self.monthField.text = [NSString stringWithFormat:@"%d", [comp month]];
+    self.dayField.text = [NSString stringWithFormat:@"%d", [comp day]];
+}
 
 - (void)viewDidLoad
 {
